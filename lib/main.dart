71f8a5709eebe690cdbb8807978d19d4d1215b3c
Joseph <jphorner22@gmail.com>
2021-10-05 15:53:56 -0500

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,88 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'RC Car Controller',
+      theme: ThemeData(
+
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'RC Car Controller'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  
+
+  // void _incrementCounter() {
+  //   setState(() {
+
+  //   });
+  // }
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+      appBar: AppBar(
+      
+        title: Text(widget.title),
+      ),
+
+        body: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          // mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                ElevatedButton(
+                  onPressed: (){},
+                  child: const Text('Left Wheel Forward')),
+                const SizedBox(height: 10,),
+                ElevatedButton(
+                  onPressed: (){},
+                  child: const Text('Left Wheel Backwards')
+                )
+              ],
+            ),
+            const SizedBox(width: 10,),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                ElevatedButton(
+                  onPressed: (){},
+                  child: const Text('Right Wheel Forward')),
+                const SizedBox(height: 10,),
+                ElevatedButton(
+                  onPressed: (){},
+                  child: const Text('Right Wheel Backwards')
+                )
+              ],
+            )
+          ],
+        ), 
+    );
+  }
+}
